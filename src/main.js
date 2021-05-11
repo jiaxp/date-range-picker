0f79bcc8d383bfd0de31c1e2df3a038b83fb3d82
贾晓朋 <jiaxp3@lenovo.com>
2021-05-11 19:32:34 +0800

    commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { DatePicker } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import DateRangePicker from '../lib/index'
+
+Vue.use(DatePicker)
+Vue.use(DateRangePicker)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
